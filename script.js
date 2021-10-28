19a4f44fcfd91e23048eea2b6602b5b924c37b50
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-10-28 21:31:25 +0600

    subtotal tax total

--- script.js
+++ script.js
@@ -8,24 +8,26 @@ function minusPlusEvent(id, plusMinus) {
     let currentValue = document.getElementById("currentInput").value;
     const currentNumber = parseFloat(currentValue);
 
-    const currentNumberUpdate = parseFloat(currentNumber + plusMinus);
+    const phoneCurrentNumberUpdate = parseFloat(currentNumber + plusMinus);
 
-    if (currentNumberUpdate < 0) {
-      document.getElementById("currentInput").value = 0;
-      document.getElementById("phone1Price").innerText = 0;
+    if (phoneCurrentNumberUpdate < 0) {
+      document.getElementById("currentInput").value = 00;
+      document.getElementById("phone1Price").innerText = 00;
     } else {
-      document.getElementById("currentInput").value = currentNumberUpdate;
+      document.getElementById("currentInput").value = phoneCurrentNumberUpdate;
       document.getElementById("phone1Price").innerText =
-        currentNumberUpdate * 1219;
+        phoneCurrentNumberUpdate * 1219;
     }
+    subtotalFunc();
   });
 }
 
 // phone section remove button
 const phoneRemoveBtn = document.getElementById("phoneRemoveBtn");
 phoneRemoveBtn.addEventListener("click", function () {
-  document.getElementById("currentInput").value = 0;
-  document.getElementById("phone1Price").innerText = "0";
+  document.getElementById("currentInput").value = 00;
+  document.getElementById("phone1Price").innerText = "00";
+  subtotalFunc();
 });
 
 //case section
@@ -42,18 +44,35 @@ function caseMinusPlusEvent(id, plusMinus) {
     const caseCurrentNumberUpdate = parseFloat(currentNumber + plusMinus);
 
     if (caseCurrentNumberUpdate < 0) {
-      document.getElementById("caseInput").value = 0;
-      document.getElementById("case1Price").innerText = 0;
+      document.getElementById("caseInput").value = 00;
+      document.getElementById("case1Price").innerText = 00;
     } else {
       document.getElementById("caseInput").value = caseCurrentNumberUpdate;
       document.getElementById("case1Price").innerText =
         caseCurrentNumberUpdate * 59;
     }
+    subtotalFunc();
   });
 }
 
 const caseRemoveBtn = document.getElementById("caseRemoveBtn");
 caseRemoveBtn.addEventListener("click", function () {
-  document.getElementById("caseInput").value = 0;
-  document.getElementById("case1Price").innerText = "0";
+  document.getElementById("caseInput").value = 00;
+  document.getElementById("case1Price").innerText = "00";
+  subtotalFunc();
 });
+
+//subtotal tax total
+const subtotalFunc = () => {
+  const subTotalValue =
+    parseFloat(document.getElementById("phone1Price").innerText) +
+    parseFloat(document.getElementById("case1Price").innerText);
+  console.log({ subTotalValue });
+
+  document.getElementById("inputSubtotal").innerText = subTotalValue;
+
+  const percentage = (5 * subTotalValue) / 100;
+  document.getElementById("inputTax").innerText = percentage;
+
+  document.getElementById("inputTotal").innerText = percentage + subTotalValue;
+};
